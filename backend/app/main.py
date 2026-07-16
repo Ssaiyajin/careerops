1e589f8e518f781f39a5836e4a98341e85ada692
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2026-07-16 21:29:54 +0200

    updating backend service to accept vercel request

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,10 @@ print("FASTAPI CREATED")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
